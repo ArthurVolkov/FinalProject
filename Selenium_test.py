e28bb4996344145bef1c023097286fe9cbbc99b8
ArthurVolkov <48453179+ArthurVolkov@users.noreply.github.com>
2019-04-26 14:43:32 +0300

    Update Selenium_test.py

--- Selenium_test.py
+++ Selenium_test.py
@@ -7,4 +7,4 @@ driver.implicitly_wait(10)    # Set the time to waiting for elements
 driver.get("http://192.168.99.101:5000")    # Open the website
 text = driver.find_element_by_xpath("//html/body")
 print(text.text)
-driver.quit()
+driver.quit()   # Close the window
