fe2dde897989382021dd91a4f84cc483f2e0f04c
ArthurVolkov <48453179+ArthurVolkov@users.noreply.github.com>
2019-04-29 21:25:28 +0300

    Update Selenium_test.py

--- Selenium_test.py
+++ Selenium_test.py
@@ -6,5 +6,5 @@ driver = webdriver.Chrome(executable_path="C:/ChD/chromedriver.exe")
 driver.implicitly_wait(10)    # Set the time to waiting for elements
 driver.get("http://192.168.99.101:5000")    # Open the website
 text = driver.find_element_by_xpath("//html/body")
-print(text.text)
+print(text.text)    # Print text
 driver.quit()   # Close the window
