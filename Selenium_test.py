54b7f30ec3f95c18f77dd01bd8631c4d72acab3b
ArthurVolkov <48453179+ArthurVolkov@users.noreply.github.com>
2019-05-04 15:40:48 +0300

    Update Selenium_test.py

--- Selenium_test.py
+++ Selenium_test.py
@@ -6,8 +6,6 @@ driver = webdriver.Chrome(executable_path="C:/ChD/chromedriver.exe")
 driver.implicitly_wait(10)    # Set the time to waiting for elements
 driver.get("http://192.168.99.101:5000")    # Open the website
 text = driver.find_element_by_xpath("//html/body")
-new_text = str(text.text)
-for char in "World":
-    new_text = new_text.replace(char, "")
+new_text = str(text.text).replace(" World", " ")
 print(new_text)
 driver.quit()   # Close the window
